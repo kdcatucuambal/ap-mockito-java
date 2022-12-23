9e885d98977e3b8472289945a67242a69a38e826
Kevin Catucuamba <kdcatucuambal@utn.edu.ec>
2022-12-23 15:29:49 -0500

    Create ExamTest.java

---
+++ src/test/java/org/kcatucuamba/appmockito/examples/models/ExamTest.java
@@ -0,0 +1,15 @@
+package org.kcatucuamba.appmockito.examples.models;
+
+import junit.framework.Assert;
+import org.junit.jupiter.api.Test;
+
+
+public class ExamTest {
+
+    @Test
+    void testExam() {
+        Exam exam = new Exam(5L, "Math");
+        Assert.assertNotNull(exam);
+    }
+
+}
