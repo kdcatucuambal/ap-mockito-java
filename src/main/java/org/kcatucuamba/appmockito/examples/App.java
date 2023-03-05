a90c8c5e4cb6d893aac152bd65330f252da823c0
Kevin Catucuamba <kdcatucuambal@utn.edu.ec>
2023-03-05 11:27:52 -0500

    Tester

--- src/main/java/org/kcatucuamba/appmockito/examples/App.java
+++ src/main/java/org/kcatucuamba/appmockito/examples/App.java
@@ -1,5 +1,9 @@
 package org.kcatucuamba.appmockito.examples;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.util.HashMap;
 import java.util.logging.Logger;
 
 /**
@@ -11,6 +15,18 @@ public class App
     private  static Logger logger = Logger.getLogger(App.class.getName());
     public static void main( String[] args )
     {
-        logger.info( "Hello World!" );
+        ObjectMapper mapper = new ObjectMapper();
+
+        HashMap<String, String> map = new HashMap<>();
+        map.put("name", "kcatucuamba");
+        map.put("age", "25");
+        String jsonResponse = "";
+        try {
+            jsonResponse = mapper.writeValueAsString(map);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+
+        logger.info(jsonResponse);
     }
 }
